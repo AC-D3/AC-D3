33b8bc608e298a1a27f3b373f2f40fc6e4a9a9b7
johnnunns <johndnunns@gmail.com>
2017-07-04 10:41:43 -0700

    added resolution logic on expand

--- acd3-homepage/js/acd3.js
+++ acd3-homepage/js/acd3.js
@@ -221,8 +221,8 @@ class acd3 {
             .on('mouseleave', (d) => this.muteOnMouseLeave(d.data))
             .on('click', (d) => this.handleSingleClick(d.data))
             .on('dblclick', (d, i) => {
-              if (this.config.onDoubleClick === 'openNewWindow') this.openNewWindow(d.data);
-              else if (this.config.onDoubleClick === 'expandBubble') this.expandBubble(d.data, i);
+                if (this.config.onDoubleClick === 'openNewWindow') this.openNewWindow(d.data);
+                else if (this.config.onDoubleClick === 'expandBubble') this.expandBubble(d.data, i);
             });
 
 
@@ -341,9 +341,24 @@ class acd3 {
         return vimeoPlayer;
     }
 
+    scaleResolution(event) {
+        
+        let youtubeIframe = document.getElementById(event.a.id);
+        console.log(youtubeIframe)
+        if (youtubeIframe.height <= this.config.resolutionThresholds[0]) {
+            event.setPlaybackQuality('small')
+        } else if (youtubeIframe.height > this.config.resolutionThresholds[0]
+            && youtubeIframe.height <= this.config.resolutionThresholds[1]) {
+            event.setPlaybackQuality('medium')
+        } else {
+            event.setPlaybackQuality('large')
+        }
+    }
+
     createYouTubePlayer(id) {
         const onYouTubePlayerReady = (event) => {
             if (this.config.autoplay) event.target.playVideo().mute();
+            // this.scaleResolution(event)
             let youtubeIframe = document.getElementById(event.target.a.id);
             if (youtubeIframe.height <= this.config.resolutionThresholds[0]) {
                 event.target.setPlaybackQuality('small')
@@ -437,19 +452,36 @@ class acd3 {
 
     }
 
+    playSolo(data) {
+        let clickedPlayer = this.playerStore[data.v_id];
+        if (data.type === 'youtube') {
+            clickedPlayer.playVideo();
+        }
+        else {
+            clickedPlayer.play();
+        }
+    }
+
     openNewWindow(data) {
         window.open(data.src);
     }
 
     expandBubble(data, i) {
         let videoID = data.v_id;
+        let clickedPlayer = this.playerStore[data.v_id];
+        console.log(clickedPlayer)
         if (this.expanded === false) {
             if (typeof InstallTrigger !== 'undefined') this.expandBubbleFirefox(data, i, videoID);
             else this.expandBubbleChrome(data, i, videoID);
+            if(data.type === 'youtube') clickedPlayer.setPlaybackQuality('hd1080')
+            this.pauseAll();
+            this.playSolo(data);
+            
             this.expanded = true
         } else {
             if (typeof InstallTrigger !== 'undefined') this.reduceBubbleFirefox(data, i, videoID);
             else this.reduceBubbleChrome(data, i, videoID);
+            this.scaleResolution(clickedPlayer)
             this.expanded = false
         }
     }
@@ -499,11 +531,11 @@ class acd3 {
             .attr('width', this.config.diameter)
             .attr('height', this.config.diameter)
 
-        d3.selectAll('.'+ this.config.htmlAnchorID + '-circle')
+        d3.selectAll('.' + this.config.htmlAnchorID + '-circle')
             .style('pointer-events', 'none')
             .style('visibility', 'hidden');
 
-       d3.selectAll('.'+ this.config.htmlAnchorID + '-video')
+        d3.selectAll('.' + this.config.htmlAnchorID + '-video')
             .style('visibility', 'hidden');
 
         let circle = d3.select('#' + this.config.htmlAnchorID + 'circleID_' + i)
@@ -568,11 +600,11 @@ class acd3 {
             .attr('x', (d) => -d.r)
             .attr('y', (d) => -d.r);
 
-        d3.selectAll('.'+ this.config.htmlAnchorID + '-circle')
+        d3.selectAll('.' + this.config.htmlAnchorID + '-circle')
             .style('pointer-events', 'auto')
             .style('visibility', 'visible');
 
-        d3.selectAll('.'+ this.config.htmlAnchorID + '-video')
+        d3.selectAll('.' + this.config.htmlAnchorID + '-video')
             .style('visibility', 'visible');
 
         d3.select('#' + videoID)
